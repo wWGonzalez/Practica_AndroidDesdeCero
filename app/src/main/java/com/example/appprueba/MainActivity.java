73811a561dd80b0fa80dc9558687f6b3684b66c1
wer <werner.e.2.g@gmail.com>
2019-07-02 08:16:35 -0600

    Suma de Dos Numeros

--- app/src/main/java/com/example/appprueba/MainActivity.java
+++ app/src/main/java/com/example/appprueba/MainActivity.java
@@ -3,25 +3,46 @@ package com.example.appprueba;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText et1;
+    private EditText et2;
+    private TextView tv1;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Integer matematica = 5;
-        Integer fisica = 5;
-        Integer lenguaje = 4;
-        Integer promedio = 0;
-        promedio = (matematica+fisica+lenguaje)/3;
 
-        if (promedio >= 6){
-            Toast.makeText(this,"Aprobado",Toast.LENGTH_SHORT).show();
-        }
-        else if(promedio <= 5){
-            Toast.makeText(this,"Reprobado",Toast.LENGTH_SHORT).show();
-        }
+        et1 = (EditText) findViewById(R.id.txtPrimerNumero);
+        et2 = findViewById(R.id.txtSegundoNumero);
+        tv1 = findViewById(R.id.textViewResultado);
+
+    }//Finaliza onCreate
+
+    //metodo que realiza suma
+    public void Suma(View view){
+        //Convierte valores EditText a String
+        String valor1 = et1.getText().toString();
+        String valor2 = et2.getText().toString();
+
+        Integer num1 = Integer.parseInt(valor1); //Convierte valores a Ingeer
+        Integer num2 = Integer.parseInt(valor2);
+
+        Integer suma = num1+num2; //Suma valores
+
+        String Resultado = String.valueOf(suma); //Convierte suma a String
+
+        tv1.setText(Resultado);//Muestra el resultado de la suma
     }
-}
+
+
+
+}//Finaliza Clase
