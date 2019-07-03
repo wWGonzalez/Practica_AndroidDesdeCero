d93accbfc88b125114bcd8d8f957938be1a88ccd
wer <werner.e.2.g@gmail.com>
2019-07-03 12:26:58 -0600

    Promedio de Tres numeros

--- app/src/main/java/com/example/appprueba/MainActivity.java
+++ app/src/main/java/com/example/appprueba/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
 
     private EditText et1;
     private EditText et2;
+    private EditText et3;
     private TextView tv1;
 
 
@@ -23,20 +24,27 @@ public class MainActivity extends AppCompatActivity {
 
         et1 = (EditText) findViewById(R.id.txtPrimerNumero);
         et2 = findViewById(R.id.txtSegundoNumero);
+        et3 = findViewById(R.id.txtTercerNumero);
         tv1 = findViewById(R.id.textViewResultado);
 
     }//Finaliza onCreate
 
 
     //metodo que realiza suma
-    public void Suma(View view){
+    public void Evaluar(View view){
         //Convierte valores EditText a String
         Integer num1 = Integer.parseInt(et1.getText().toString());
         Integer num2 = Integer.parseInt(et2.getText().toString());
+        Integer num3 = Integer.parseInt(et3.getText().toString());
 
-        Integer suma = num1+num2; //Suma valores
+        Integer promedio = (num1+num2+num3)/3; //Suma valores
+        if(promedio>=6){
+            tv1.setText("Aprobado con: "+promedio.toString());
 
-        tv1.setText(suma.toString());//Muestra el resultado de la suma
+        }else if(promedio<5){
+            tv1.setText("Reprobado con :"+promedio.toString());
+        }
+
     }
 
 
