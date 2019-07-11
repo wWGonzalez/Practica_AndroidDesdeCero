75d7739ca4a52a285298f1555f2b6605f120e748
wer <werner.e.2.g@gmail.com>
2019-07-11 15:33:51 -0600

    Control ListView Video 20

--- app/src/main/java/com/example/appprueba/MainActivity.java
+++ app/src/main/java/com/example/appprueba/MainActivity.java
@@ -4,9 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.RadioButton;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -14,10 +16,12 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText et1;
-    private EditText et2;
-    private Spinner spinner1;
-    private TextView tv1;
+    private TextView tv1; //TexView
+    private ListView lv1; //ListView
+
+    private String nombres [] = {"Samuel","Sofia","Marta","Valentina"};
+    private String edades [] = {"18","17","24","20"};
+
 
 
 
@@ -26,56 +30,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         this.Componetes();
-        this.Spiner();
 
 
-    }//Finaliza onCreat this.obtenerComponentes();e
+    }//Finaliza onCreate
 
 
-    //Etablecer Datos en Spiner
-    private void Spiner(){
-        String [] opciones = {"sumar","restar","multriplicar","dividir"};
-        ArrayAdapter <String> adapter = new ArrayAdapter<String>(this,R.layout.spinner_item,opciones);
-        spinner1.setAdapter(adapter);
-    }
 
     //Obtener Componentes
     private void Componetes(){
-        et1 = findViewById(R.id.txtPrimerNumero);
-        et2 = findViewById(R.id.txtSegundoNumero);
-        tv1 = findViewById(R.id.textViewResultado);
-        spinner1 = findViewById(R.id.spinner);
+        tv1 = findViewById(R.id.textView1);
+        lv1 = findViewById(R.id.listView1);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.list_item_nombres, nombres);
+        lv1.setAdapter(adapter);
+        lv1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                tv1.setText("La edad de "+lv1.getItemAtPosition(i)+" Es:"+edades[i]+" Años");
+            }
+        });
     }//Finaliza Obtener Componetes
 
 
-    public void Evaluar(View view){
-        Integer num1 = Integer.parseInt(et1.getText().toString());
-        Integer num2 = Integer.parseInt(et2.getText().toString());
-        Integer resultado=0;
-
-        String seleccion= spinner1.getSelectedItem().toString();
-        if(seleccion.equals("sumar")){
-            resultado = num1+num2;
-            tv1.setText(resultado.toString());
-        }else if(seleccion.equals("restar")){
-             resultado=num1-num2;
-            tv1.setText(resultado.toString());
-        }else if(seleccion.equals("multriplicar")){
-            resultado=num1*num2;
-            tv1.setText(resultado.toString());
-        }else if(seleccion.equals("dividir")){
-            if(num2.equals(0)){
-                Toast.makeText(this,"No se Puede dividir",Toast.LENGTH_SHORT).show();
-            }
-            else{
-                resultado=num1/num2;
-                tv1.setText(resultado.toString());
-            }
-
-        }
 
 
-    }//Finaliza evaluar
 
 
 
